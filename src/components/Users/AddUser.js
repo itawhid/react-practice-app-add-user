bcc6a1ff1aa604c4d73bb627c31dc6893ba9194b
Tawhidul Islam <tawhidul.islam@spaceteams.de>
2022-12-26 18:14:31 +0100

    feat: Added add user

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,18 @@
+const AddUser = () => {
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">User Name</label>
+      <input id="username" type="text"/>
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number"/>
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
